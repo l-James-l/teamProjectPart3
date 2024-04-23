b182899f47fda8f0d4d902a15daae33882733970
l-James-l <77617382+l-James-l@users.noreply.github.com>
2024-04-23 13:43:55 +0000

    made header links work

--- src/header.php
+++ src/header.php
@@ -19,7 +19,7 @@ if (!isset($currentPage)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Make It All</title>
-	<link rel="icon" type="image/x-icon" href="./imgs/logo.ico">
+	<link rel="icon" type="image/x-icon" href="../src/imgs/logo.ico">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
@@ -32,14 +32,14 @@ if (!isset($currentPage)) {
 <body>
     <header>
         <div class="container header-container">
-            <img src="./imgs/logo.png" alt="Company Logo" id="page-logo">
+            <img src="../src/imgs/logo.png" alt="Company Logo" id="page-logo">
 
             <ul style="display: flex;">
                 <li class="sub-system-link <?php echo $currentPage=='analytics' ? 'current-page-li': '' ?>">
                     <a class="headerLink <?php echo $currentPage=='analytics' ? 'current-page': 'default' ?>" href="../analytics_landing_page.php?lf=projects">Analytics</a>
                 </li>
                 <li class="sub-system-link <?php echo $currentPage=='chat' ? 'current-page': '' ?>">
-                    <a class="headerLink <?php echo $currentPage=='chat' ? 'current-page': 'default' ?>" href="../textchat/index2.html.php">Chat</a>
+                    <a class="headerLink <?php echo $currentPage=='chat' ? 'current-page': 'default' ?>" href="../textchat/index2.html">Chat</a>
                 </li>
             </ul>
 
@@ -49,13 +49,13 @@ if (!isset($currentPage)) {
 
             <div class=" dropdown">
                 <a href="#" class="d-block link-dark text-decoration-none user-dropdown dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="./imgs/icon.png" alt="mdo" width="42" height="42" class="rounded-circle">
+                    <img src="../src/imgs/icon.png" alt="mdo" width="42" height="42" class="rounded-circle">
                 </a>
                 <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
                     <li>
                         <div class="dropdown-item dropdown-item-nohover" >
                             <div style="white-space: normal;">
-                                <img src="./imgs/icon.png" alt="mdo" width="32" height="32" class="rounded-circle">
+                                <img src="../src/imgs/icon.png" alt="mdo" width="32" height="32" class="rounded-circle">
                                 <span style="padding-left: 10px;"><?php echo $_SESSION["first_name"] . " " . $_SESSION["surname"]?></span>
                             </div>
                             <span><?php echo $_SESSION["email"]?></span>
