e741a8653042260474942e776f64a1c18e99de2a
jhb33 <148326458+jhb33@users.noreply.github.com>
2024-04-27 14:36:52 +0000

    Changed request method to POST

--- textchat/delete-message.php
+++ textchat/delete-message.php
@@ -3,12 +3,12 @@ include_once(__DIR__ . '/../src/db_connection.php');
 $connection=mysqli_connect($servername,$username,$password,$dbname);
 $statement=mysqli_stmt_init($connection);
 
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-    if(isset($_GET['message_id'])&& !empty($_GET['message_id'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(isset($_POST['message_id'])&& !empty($_POST['message_id'])) {
         mysqli_stmt_prepare($statement,"DELETE FROM chat_log
         WHERE message_id=? 
         AND user_id=?");
-        mysqli_stmt_bind_param($statement,"ii",$_GET['message_id'],$_SESSION['user_id']);
+        mysqli_stmt_bind_param($statement,"ii",$_POST['message_id'],$_SESSION['user_id']);
         mysqli_stmt_execute($statement);
         $affectedRows=mysqli_stmt_affected_rows($statement);
         if($affectedRows==1) {
