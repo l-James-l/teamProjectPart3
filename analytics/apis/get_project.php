0157d307d60acf3e6da2811b4d7c813141b29fda
l-James-l <77617382+l-James-l@users.noreply.github.com>
2024-04-30 00:20:57 +0100

    Create get_project.php

---
+++ analytics/apis/get_project.php
@@ -0,0 +1,34 @@
+<?php
+include "../../src/db_connection.php";
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=make_it_all", $username, $password);
+} catch (PDOException $e) {
+    echo "<script type='text/javascript'>alert('Failed to connect to database');</script>";
+}
+
+if ($conn) {
+    if (isset($_GET["project_ID"])) {
+        $final_json = json_decode("{}");
+
+        $stmt = "select * from project where project_id = :project_id";
+        $query = $conn->prepare($stmt);
+        $query->bind_param(":project_id", $_GET["project_ID"]);
+        $result = $query->execute();
+        if ($result) {
+            $final_json["project"] = $query->fetch();
+            $stmt = " select * from task where project_id = :project_id";
+            $query = $conn->prepare($stmt);
+            $query->bind_param(":project_id", $_GET["project_ID"]);
+            $result = $query->execute();
+            if ($result) {
+                $final_json["tasks"] = $query->fetchAll();
+                echo json_encode(array("status" => "success", "message" => $final_json));
+                exit;
+            }
+        }
+    } 
+}
+
+echo json_encode(array("status" => "error", "message" => null));
+
+?>
