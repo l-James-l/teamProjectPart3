69736f138e27df7db04e6f167c8fcd0af2e4e977
owaller19 <94453015+owaller19@users.noreply.github.com>
2024-05-01 14:34:43 +0100

    Update individual_analytics.php

--- analytics/individual_analytics.php
+++ analytics/individual_analytics.php
@@ -103,7 +103,7 @@ if (isset($_GET['userToGet'])) {
 
                     $sql = "SELECT t.task_name, p.project_name, t.due_date, t.priority, t.estimated_length, t.completion_percentage 
                             FROM task t 
-                            INNER JOIN projects p ON t.projectID = p.projectID
+                            INNER JOIN project p ON t.projectID = p.projectID
                             WHERE t.userID = ?";
                     $stmt = $conn->prepare($sql);
                     if ($stmt === false) {
