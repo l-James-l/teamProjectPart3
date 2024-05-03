c1608c6098e9cb3990bde2a7d08c8b1133a35066
l-James-l <77617382+l-James-l@users.noreply.github.com>
2024-05-03 16:07:10 +0000

    typo

--- analytics/apis/get_project.php
+++ analytics/apis/get_project.php
@@ -95,7 +95,7 @@ if (isset($_GET["search"])) {
     if (count($search_strings) > 0) {
         $stmt = $stmt . " and ("; 
         foreach ($search_strings as $ss) {
-            if ($ss != "" || $ss != " ") {
+            if ($ss != "" && $ss != " ") {
                 $stmt = $stmt . "concat(first_name, ' ', surname) like '%$ss%' or task_title like '%$ss%' or ";
             }
         }
