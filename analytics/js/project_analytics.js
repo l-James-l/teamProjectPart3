85be2b0b0d1fd36f74f59c0daf075a87b5760076
l-James-l <77617382+l-James-l@users.noreply.github.com>
2024-05-04 22:01:26 +0000

    visual changes

--- analytics/js/project_analytics.js
+++ analytics/js/project_analytics.js
@@ -120,8 +120,9 @@ function drawHoursBarChart(userData) {
         collapse_button.innerHTML = username
 
         let user_link_button = document.createElement("button")
-        user_link_button.href = "./individual_analytics.php?userToGet="+userData[username]["user_id"]
-        user_link_button.classList.add("btn")
+        user_link_button.onclick = document.location = "./individual_analytics.php?userToGet="+userData[username]["user_id"]
+        user_link_button.classList.add("btn", "btn-primary")
+        user_link_button.style["margin-left"] = "30px"
         user_link_button.innerHTML = "View User Details"
         collapse_button.appendChild(user_link_button)
 
