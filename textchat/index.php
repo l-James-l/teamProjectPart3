8a841c0ef89479f875abf100243c38ff4c6000e7
Dom Parsons <69306241+domparsons@users.noreply.github.com>
2024-05-04 19:18:49 +0000

    Styling change

--- textchat/index.php
+++ textchat/index.php
@@ -14,8 +14,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Messaging Service</title>
-    <!-- <link rel="stylesheet" href="stylesheets/messaging-styles-colour.css"> -->
-    <link rel="stylesheet" href="stylesheets/messaging-styles.css">
+    <link rel="stylesheet" href="stylesheets/messaging-styles-colour.css">
+    <!-- <link rel="stylesheet" href="stylesheets/messaging-styles.css"> -->
 </head>
 <body>
     <header class="navbar">
