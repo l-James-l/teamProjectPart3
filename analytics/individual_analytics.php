f8d17323ef9d1458cc87ff008b9dee3cff5d8dd7
owaller19 <94453015+owaller19@users.noreply.github.com>
2024-05-06 15:02:23 +0100

    Update individual_analytics.php

--- analytics/individual_analytics.php
+++ analytics/individual_analytics.php
@@ -23,7 +23,7 @@ if (isset($_GET['userToGet'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Analytics Dashboard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="stylesheets/user_analytics.css">
+    <link rel="stylesheet" href="stylesheets/individual_analytics.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/individual_handler.js"></script>
 </head>
