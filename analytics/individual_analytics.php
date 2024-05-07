2aaa787dab0433f4f8eb1235942599b94b77b1fb
owaller19 <94453015+owaller19@users.noreply.github.com>
2024-05-07 12:10:31 +0100

    Add files via upload

--- analytics/individual_analytics.php
+++ analytics/individual_analytics.php
@@ -22,7 +22,7 @@ if (isset($_GET['userToGet'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Analytics Dashboard</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="stylesheets/user_analytics.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/individual_handler.js"></script>
