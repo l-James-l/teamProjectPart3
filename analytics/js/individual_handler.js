88ca4f0dd581aa2c8316190d4983d855a8ab81bd
owaller19 <94453015+owaller19@users.noreply.github.com>
2024-05-07 11:03:21 +0100

    Add files via upload

--- analytics/js/individual_handler.js
+++ analytics/js/individual_handler.js
@@ -1,5 +1,5 @@
 function fetchUserData(userID, callback) {
-    fetch(`apis/getUserDetails.php?userID=${userID}`)
+    fetch(`api/getUserDetails.php?userID=${userID}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -15,22 +15,22 @@ function fetchUserData(userID, callback) {
         });
 }
 
-function updateUserData(userData){
+function updateUserData(userData) {
     document.getElementById('fullName').innerText = userData.data.userDetails.fullName;
     document.getElementById('role').innerText = userData.data.userDetails.role;
 
-    document.getElementById('percentageNumber').innerText = userData.data.statistics.overallCompletion;
-    document.getElementById('percentageNumber').setAttribute('title', `Hours Done: ${userData.data.statistics.hoursDone}, Hours Left: ${userData.data.statistics.hoursLeft}`);
-    document.getElementById('hoursNumber').innerText = userData.data.statistics.hoursLeft;
-    document.getElementById('taskProjectInfo').innerText = `Current Tasks - ${userData.data.statistics.taskCount} across ${userData.data.statistics.projectCount} projects`;
+    var completionPercentage = userData.data.statistics.overallCompletion;
+    var percentageElement = document.getElementById('percentageNumber');
 
-    var circlePercentageElement = document.getElementById('circlePercentage');
-    if (userData.data.statistics.overallCompletion < 40) {
-        circlePercentageElement.style.backgroundColor = 'red';
-    } else if (userData.data.statistics.overallCompletion >= 40 && userData.data.statistics.overallCompletion <= 70) {
-        circlePercentageElement.style.backgroundColor = 'yellow';
+    document.getElementById('hoursNumber').innerText = userData.data.statistics.hoursLeft;
+    document.getElementById('taskProjectInfo').innerText = `${userData.data.statistics.taskCount} tasks assigned across ${userData.data.statistics.projectCount} projects`;
+
+    if (completionPercentage < 40) {
+        percentageElement.style.color = 'red';
+    } else if (completionPercentage >= 40 && completionPercentage <= 70) {
+        percentageElement.style.color = 'yellow';
     } else {
-        circlePercentageElement.style.backgroundColor = 'green';
+        percentageElement.style.color = 'green';
     }
 }
 
