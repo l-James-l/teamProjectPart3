24951120c397103f5a50b7517c0a303a7a9e1d6d
Dom Parsons <69306241+domparsons@users.noreply.github.com>
2024-05-09 08:57:49 +0000

    Send message SVG

--- textchat/index.php
+++ textchat/index.php
@@ -50,7 +50,12 @@ session_start();
                 <input type="hidden" name="chat_id" id="chat_id" value="">
                 <textarea name="message" id="message" placeholder="Type your message..." class="send-message-field-textarea"></textarea>
 
-                <button type="submit" id="send-message-button">Send message</button>
+                <button type="submit" id="send-message-button">
+                    <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115.4 122.88">
+                        <title>up-arrow</title>
+                        <path d="M24.94,67.88A14.66,14.66,0,0,1,4.38,47L47.83,4.21a14.66,14.66,0,0,1,20.56,0L111,46.15A14.66,14.66,0,0,1,90.46,67.06l-18-17.69-.29,59.17c-.1,19.28-29.42,19-29.33-.25L43.14,50,24.94,67.88Z"/>
+                    </svg>
+                </button>
             </form>
 
             <div id="editMessageModal" class="modal">
@@ -60,12 +65,7 @@ session_start();
                         <input type="hidden" id="editMessageId">
                         <label for="editMessageText">Edit Message:</label>
                         <input type="text" id="editMessageText" name="editMessageText">
-                        <button type="submit" id="sendButton">
-                            <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 115.4 122.88">
-                                <title>up-arrow</title>
-                                <path d="M24.94,67.88A14.66,14.66,0,0,1,4.38,47L47.83,4.21a14.66,14.66,0,0,1,20.56,0L111,46.15A14.66,14.66,0,0,1,90.46,67.06l-18-17.69-.29,59.17c-.1,19.28-29.42,19-29.33-.25L43.14,50,24.94,67.88Z"/>
-                            </svg>
-                        </button>
+                        <button type="submit">Save</button>
                     </form>
                 </div>
             </div>
