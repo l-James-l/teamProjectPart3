e4ef6ec481d655aaf8715e5e917d41364a82bdf5
Dom Parsons <69306241+domparsons@users.noreply.github.com>
2024-05-09 08:35:57 +0000

    Smaller icon and delete functionality

--- textchat/index.php
+++ textchat/index.php
@@ -159,14 +159,12 @@ session_start();
 
             // Add edit and delete buttons for outgoing messages
             if (type === 'outgoing') {
-                var deleteBtnSVG = `<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256">
-                <g fill="#ff0000" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
-                <g transform="scale(8.53333,8.53333)">
-                <path d="M14.98438,2.48633c-0.55152,0.00862 -0.99193,0.46214 -0.98437,1.01367v0.5h-5.5c-0.26757,-0.00363 -0.52543,0.10012 -0.71593,0.28805c-0.1905,0.18793 -0.29774,0.44436 -0.29774,0.71195h-1.48633c-0.36064,-0.0051 -0.69608,0.18438 -0.87789,0.49587c-0.18181,0.3115 -0.18181,0.69676 0,1.00825c0.18181,0.3115 0.51725,0.50097 0.87789,0.49587h18c0.36064,0.0051 0.69608,-0.18438 0.87789,-0.49587c0.18181,-0.3115 0.18181,-0.69676 0,-1.00825c-0.18181,-0.3115 -0.51725,-0.50097 -0.87789,-0.49587h-1.48633c0,-0.26759 -0.10724,-0.52403 -0.29774,-0.71195c-0.1905,-0.18793 -0.44836,-0.29168 -0.71593,-0.28805h-5.5v-0.5c0.0037,-0.2703 -0.10218,-0.53059 -0.29351,-0.72155c-0.19133,-0.19097 -0.45182,-0.29634 -0.72212,-0.29212zM6,9l1.79297,15.23438c0.118,1.007 0.97037,1.76563 1.98438,1.76563h10.44531c1.014,0 1.86538,-0.75862 1.98438,-1.76562l1.79297,-15.23437z"></path>
-                </g>
-                </g>
+                var deleteBtnSVG = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="#ff0000">
+                <path d="M0 0h24v24H0z" fill="none"/>
+                <path d="M0 0h24v24H0V0z" fill="none"/>
+                <path d="M9 16h6c.55 0 1-.45 1-1s-.45-1-1-1H9c-.55 0-1 .45-1 1s.45 1 1 1zM19 4h-3.17L15 2H9L7.17 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.89 2 1.99 2H19c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H5V8h14v10z"/>
                 </svg>`;
-                
+
                 var deleteBtn = document.createElement("div");
                 deleteBtn.innerHTML = deleteBtnSVG;
                 deleteBtn.classList.add("delete-button");
@@ -179,6 +177,7 @@ session_start();
                 messageDiv.appendChild(editBtn);
             }
         }
+
 
 
 
