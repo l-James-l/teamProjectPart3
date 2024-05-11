5f9398c5ff050ce6a1ef6e6f4a44088d9dc2b966
jhb33 <148326458+jhb33@users.noreply.github.com>
2024-05-11 14:17:10 +0000

    Error reporting on

--- textchat/fetch-user-list.php
+++ textchat/fetch-user-list.php
@@ -1,5 +1,8 @@
 <?php
 include_once(__DIR__ . '/../src/db_connection.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 $connection=mysqli_connect($servername,$username,$password,$dbname);
 $statement=mysqli_stmt_init($connection);
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
