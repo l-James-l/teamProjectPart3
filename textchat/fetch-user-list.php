953898d939d66417ee5f41363b64711d62dbaeb7
jhb33 <148326458+jhb33@users.noreply.github.com>
2024-05-11 14:34:50 +0000

    Further testing

--- textchat/fetch-user-list.php
+++ textchat/fetch-user-list.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         $statement=mysqli_stmt_init($connection);
         $success=mysqli_stmt_prepare($statement,"SELECT user_id, first_name, surname 
         FROM user;");
-        if(!statement) {
+        if(!$statement) {
             echo "error";
         }
         if(!$success) {
