77f754af5d29262976328770e67a8e0f6a976083
jhb33 <148326458+jhb33@users.noreply.github.com>
2024-05-11 18:57:34 +0000

    Testing

--- textchat/create-chat.php
+++ textchat/create-chat.php
@@ -32,6 +32,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     $isAdmin=false;
                 }
                 if($chatNameGiven) {
+                    echo "running - name given";
                     mysqli_stmt_prepare($chatCreateStatement,"INSERT INTO chat(chat_name,is_group)
                     VALUES (?,?)");
                     mysqli_stmt_bind_param($chatCreateStatement,"si",$chatName,$isGroup);
