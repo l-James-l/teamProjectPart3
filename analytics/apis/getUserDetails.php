72ebecb5f1dd9478d2757fae631619a582f249d7
l-James-l <77617382+l-James-l@users.noreply.github.com>
2024-05-12 23:02:44 +0000

    test

--- analytics/apis/getUserDetails.php
+++ analytics/apis/getUserDetails.php
@@ -79,7 +79,7 @@ $taskCount = count($projects);
 // get the progress log
 $sql = "SELECT date, sum(hours_logged) AS hours_sum 
 FROM task_progress_log
-WHERE user_id = 1   
+-- WHERE user_id = 1   
 GROUP BY date 
 ORDER BY date";
 
