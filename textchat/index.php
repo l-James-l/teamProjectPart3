90032b64d6cf86d681d1bfccb798310c1e8c1ac5
D0na1 <147496848+D0na1@users.noreply.github.com>
2024-05-14 01:20:52 +0000

    rev

--- textchat/index.php
+++ textchat/index.php
@@ -469,8 +469,7 @@ session_start();
                     chatName.textContent = chat.chat_name;
 
                     // Append chat name to the chat preview
-                    if chatPreview.appendChild(chatName);
-                    
+                    chatPreview.appendChild(chatName);
 
                     // Add an event listener to load the chat messages when clicked
                     chatPreview.addEventListener('click', function() {
